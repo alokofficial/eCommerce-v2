d0077271ae0f91e919cf1f6664fe1b93c51d4e3b
Alok Kumar <aloknh31@gmail.com>
2025-01-15 12:10:52 +0530

    implementing react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 function App() {
   return (
     <>
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container> 
       </main>
       <Footer />
