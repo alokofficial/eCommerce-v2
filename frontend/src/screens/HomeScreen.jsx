373e217616ce0c382560cab4d51a05edc39222f7
Alok Kumar <aloknh31@gmail.com>
2025-01-16 04:10:19 +0530

    implementation of Redux Store, apiSlice and productSlice

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,33 +1,26 @@
-import { useState,useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-// import products from "../products";
+import { useGetProductQuery } from "../slices/productSlice";
 import Product from "../components/Product";
-import axios from "axios";
-
-
-
 const HomeScreen = () => {
-
-  const [products, setProducts] = useState([]);
-
-  useEffect(()=>{
-    const fetchProducts = async () =>{
-      const {data} = await axios.get('/api/products');
-      setProducts(data);
-    }
-    fetchProducts();
-  },[])
-
+  const { data: products, isLoading, error } = useGetProductQuery();
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error?.data.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
