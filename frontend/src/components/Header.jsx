47d3e05225fe14d515d0741d6b562bd0603eac7d
Alok Kumar <aloknh31@gmail.com>
2025-01-15 11:27:59 +0530

    logo and custom css added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { Navbar, Nav, Container } from "react-bootstrap"
 import {FaShoppingCart, FaUser} from "react-icons/fa"
+import logo from "../assets/logo.png"
 const Header = () => {
   return (
     <header>
-        <Navbar bg="success" variant="dark" expand="lg" collapseOnSelect>
-            <Container>
-                <Navbar.Brand href="/">EpoxySneak</Navbar.Brand>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect className="py-2 shadow">
+            <Container fluid className="mx-1">
+                <Container>
+                    <Navbar.Brand href="/"><img src={logo} alt="logo" className="rounded-circle" width="60px" /></Navbar.Brand>
+                    <Navbar.Brand href="/" className="text-uppercase fw-bold">EpoxySneak</Navbar.Brand>
+                </Container>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto align-items-end">
