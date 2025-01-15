0d2bbf2f030bed5d21906f21d2cc2ef989db9a1c
Alok Kumar <aloknh31@gmail.com>
2025-01-16 03:00:51 +0530

    added error middleware

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const app = express();
 import connectDB from "./config/db.js";
 connectDB()
 import productRouter from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,8 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 })
 
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
 });
