00e22ad00a5aa6e052d8302cb55130eb1ad8ffb2
Alok Kumar <aloknh31@gmail.com>
2025-01-15 11:07:39 +0530

    added header and footer initial components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 function App() {
   return (
-    <div>App component</div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Home</h1>
+        </Container> 
+      </main>
+      <Footer />
+    </>
   );
 }
 export default App;
