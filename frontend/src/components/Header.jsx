f3341894742abbf111da52abf4a53d42d46bf1fe
Alok Kumar <aloknh31@gmail.com>
2025-01-15 12:58:54 +0530

    added link for the routing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,25 @@
 import { Navbar, Nav, Container } from "react-bootstrap"
 import {FaShoppingCart, FaUser} from "react-icons/fa"
 import logo from "../assets/logo.png"
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect className="py-2 shadow">
             <Container fluid className="mx-1">
-                <Container>
-                    <Navbar.Brand href="/"><img src={logo} alt="logo" className="rounded-circle" width="60px" /></Navbar.Brand>
-                    <Navbar.Brand href="/" className="text-uppercase fw-bold">EpoxySneak</Navbar.Brand>
-                </Container>
+                
+                    <Navbar.Brand >
+                        <Link to="/home">
+                            <img src={logo} alt="logo" className="rounded-circle" width="50px" />
+                            <span className="text-uppercase fw-bold ms-2">EpoxySneak</span>
+                        </Link>
+                    </Navbar.Brand>
+                
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto align-items-end">
-                        <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                        <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
+                            <Link to="/cart" className="nav-link"><FaShoppingCart /> Cart</Link>
+                            <Link to="/login" className="nav-link"><FaUser /> Sign In</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
