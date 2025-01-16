a2866efdc1c498e4114bc431aae216b1010f89b0
Alok Kumar <aloknh31@gmail.com>
2025-01-16 05:38:37 +0530

    Add and remove from cart functionality added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,28 @@
-import { Navbar, Nav, Container } from "react-bootstrap"
+import { Navbar, Nav, Container, Badge } from "react-bootstrap"
 import {FaShoppingCart, FaUser} from "react-icons/fa"
 import logo from "../assets/logo.png"
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 const Header = () => {
+    const {cartItems} = useSelector(state => state.cart)
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect className="py-2 shadow">
             <Container fluid className="mx-1">
-                
                     <Navbar.Brand >
                         <Link to="/">
                             <img src={logo} alt="logo" className="rounded-circle" width="50px" />
                             <span className="text-uppercase fw-bold ms-2">EpoxySneak</span>
                         </Link>
                     </Navbar.Brand>
-                
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto align-items-end">
-                            <Link to="/cart" className="nav-link"><FaShoppingCart /> Cart</Link>
+                            <Link to="/cart" className="nav-link"><FaShoppingCart /> Cart
+                            {cartItems.length > 0 && (
+                                <Badge pill bg="danger" className="ms-1">{cartItems.reduce((acc, item) => acc + item.qty, 0)}</Badge>
+                            )}
+                            </Link>
                             <Link to="/login" className="nav-link"><FaUser /> Sign In</Link>
                     </Nav>
                 </Navbar.Collapse>
