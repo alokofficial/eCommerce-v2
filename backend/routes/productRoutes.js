96555ed55c115258537a513995ca6d3d7c069fcf
Alok Kumar <aloknh31@gmail.com>
2025-01-16 22:16:31 +0530

    Create product with sample data

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById } from "../controllers/productController.js";
+import { getProducts, getProductById, createProduct } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.get('/', getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 router.get('/:id',getProductById)
 
