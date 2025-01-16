183ab8866b9681898144c335570c42272823aea3
Alok Kumar <aloknh31@gmail.com>
2025-01-16 18:25:59 +0530

    Add expiration time of user in frontend

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -13,10 +13,13 @@ const initialState = {
         setCredentials:(state,action)=>{
             state.userInfo = action.payload;
             localStorage.setItem("userInfo",JSON.stringify(action.payload));
+            const expirationTime = new Date().getTime() +  60 * 60 * 1000; // 1 hour
+            localStorage.setItem("expirationTime", expirationTime);
         },
         logout:(state,action)=>{
             state.userInfo = null;
             localStorage.removeItem("userInfo");
+            localStorage.removeItem("expirationTime");
         }
     },
   })
