8b2f960fc85fe421919c7b36c701ca94eab30afa
Alok Kumar <aloknh31@gmail.com>
2025-01-16 12:01:17 +0530

    Payment screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import CartScreen from './screens/CartScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
 import ShippingScreen from './screens/ShippingScreen.jsx';
+import PaymentScreen from './screens/PaymentScreen.jsx';
 import store from './store.js';
 import { Provider } from 'react-redux';
 
@@ -30,6 +31,7 @@ const router = createBrowserRouter(
       <Route path='/register' element={<RegisterScreen />} />
       <Route path='' element={<PrivateRoute />} >
         <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
       </Route>
     </Route>
   )
